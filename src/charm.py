e5f5f9f0e447e46890ed926c376aa4cc5d66ddfe
ducks23 <jesse1517@gmail.com>
2020-08-10 17:47:30 +0000

    linted

--- src/charm.py
+++ src/charm.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
 """ElasticsearchCharm."""
 import logging
+import os
+from pathlib import Path
+import socket
+import subprocess
+
+from elasticsearch_interface import ElasticSearchInterface
+from jinja2 import Environment, FileSystemLoader
 from ops.charm import CharmBase
 from ops.main import main
-from ops.model import (
-    ActiveStatus,
-    BlockedStatus,
-)
-from jinja2 import Environment, FileSystemLoader
-import subprocess
-from pathlib import Path
-from ops.framework import StoredState
-import socket
-import os
-from elasticsearch_interface import ElasticSearchInterface
+from ops.model import ActiveStatus
 
 logger = logging.getLogger()
 
+
 class ElasticsearchCharm(CharmBase):
-    """Operator charm responsible for lifecycle operations of elasticsearch"""
-    _stored = StoredState()
+    """Operator charm for Elasticsearch."""
+
     def __init__(self, *args):
         """Initialize charm, configure states, and events to observe."""
         super().__init__(*args)
-        self._stored.set_default(
-            installed = False,
-        )
         self.elastic_search = ElasticSearchInterface(self, "elasticsearch")
-
         event_handler_bindings = {
             self.on.install: self._on_install,
             self.on.start: self._on_start,
@@ -36,23 +30,25 @@ class ElasticsearchCharm(CharmBase):
             self.framework.observe(event, handler)
 
     def _on_install(self, event):
-        """Install ElasticSearch from cli"""
+        """Install ElasticSearch."""
         subprocess.run(
-            ["sudo", "dpkg", "-i", self.model.resources.fetch("elasticsearch")
-        ])
+            ["sudo", "dpkg", "-i", self.model.resources.fetch("elasticsearch")]
+        )
         host_name = socket.gethostname()
         ctxt = {"hostname": host_name}
         write_config(ctxt)
         open_port(9200)
-
+        self.unit.status = ActiveStatus("Elasticsearch Installed")
 
     def _on_start(self, event):
+        """Start Elasticseach."""
         subprocess.run(
             ["sudo", "-i", "service", "elasticsearch", "start"])
 
 
 def write_config(context):
     """Render the context to a template.
+
     target: /etc/elasticsearch/elasticsearch.yml
     source: /templates/elasticsearch.yml.tmpl
     file name can also be slurmdbdb.conf
@@ -82,8 +78,11 @@ def _modify_port(start=None, end=None, protocol='tcp', hook_tool="open-port"):
         port = ""
     subprocess.run([hook_tool, f"{port}{protocol}"])
 
+
 def open_port(start, end=None, protocol="tcp"):
+    """Open port in operator framework."""
     _modify_port(start, end, protocol=protocol, hook_tool="open-port")
+
 
 if __name__ == "__main__":
     main(ElasticsearchCharm)
--- src/elasticsearch_interface.py
+++ src/elasticsearch_interface.py
@@ -1,30 +1,34 @@
 #! /usr/bin/env python3
-"""SlurmdbdProvidesRelation."""
-import logging
+"""ElasticsearchProvidesRelation."""
 import socket
+
 from ops.framework import (
     EventBase,
-    EventSource,
     Object,
     ObjectEvents,
-)   
+)
+
 
 class ElasticSearchAvailableEvent(EventBase):
-    pass
+    """Available Event."""
+
 
 class ElasticSearchEvents(ObjectEvents):
-    pass
+    """Interface Events."""
+
 
 class ElasticSearchInterface(Object):
-    """Connect Slurmctld to elasticsearch"""
-    def __init(self, charm, relation_name):
-        """set the provide relation data"""
-        super().__init__(charm, relation_name)
+    """Connect Slurmctld to elasticsearch."""
 
+    def __init(self, charm, relation_name):
+        """Set the provide relation data."""
+        super().__init__(charm, relation_name)
         self.charm = charm
         self.framework.observe(
             charm.on[self._relation_name].relation_created,
             self._on_relation_created
         )
+
         def _on_relation_created(self, event):
+            """Set data on relation created."""
             event.relation.data[self.model.unit]['hostname'] = socket.gethostname().split(".")[0]
